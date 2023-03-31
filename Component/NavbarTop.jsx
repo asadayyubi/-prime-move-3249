79e035b879dec5b513db540869720826fab1b60d
asadayyubi <asad.ayyubi25@gmail.com>
2023-03-31 15:52:33 +0530

    bug fix

---
+++ Component/NavbarTop.jsx
@@ -0,0 +1,48 @@
+
+import React from 'react'
+import {
+    Box,
+    Flex,
+    Image,
+    Link,
+    Spacer,
+    Text
+  } from '@chakra-ui/react';
+  import { AiFillGift} from "react-icons/ai";
+ const NavbarTop = () => {
+  return (
+    <Box  width='100%' bg='#fee8e8' height='50px' display='flex' justifyContent='center' alignItems='center' pl='-3px' >       
+        <Flex width='70%' margin='auto'  >
+            <Box>
+                <AiFillGift  style={{color: 'orange', border:'1px', fontSize: '24px'}} />
+            </Box>
+
+            <Box className="topnavoffer" fontFamily='sans-serif' color={"orange.600"} fontWeight='500' fontSize='13px' pl='13px' >  
+            LIt Liquid Matte Lipstick + LIT Velvet Matte Liquid Lipstick GET this @Re 1
+            </Box>
+
+
+            <Box  fontFamily='sans-serif'  color= '#ec4899' fontWeight='bold' fontSize='13px' pl='10px' >
+
+            <Box  fontFamily='sans-serif'  color= '#ec4899' fontSize='13px' pl='10px' >
+
+                SHOP NOW
+            </Box>
+            <Spacer/>
+
+            <Link href='https://www.myglamm.com/store-locator' >
+            <Box fontFamily='sans-serif'  fontSize='13px'  >
+         
+                <Flex fontSize={"13px"} width={"150px"} gap={3}><Image width={"20px"} 
+                 color="orange" src="https://cdn3.iconfinder.com/data/icons/linecons-free-vector-icons-pack/32/shop-512.png"></Image>
+                 
+                <Text color={"orange.900"} fontWeight={'bold'} mr={-10}>MY GG STORE</Text></Flex>
+            </Box></Link>
+
+            </Box>
+        </Flex>       
+    </Box>
+  );
+}
+export default NavbarTop
+
--- README.md
+++ README.md
@@ -1,1 +1,59 @@
-# -prime-move-3249
+# Grow Glamorous (GG.com)
+
+ ![Logo](/images/GGNew.png)
+
+GG.com is the clone of Myglamm , a E-commerce site. This is a shopping website of cosmetic products belongs to women such as makeup product, lipstick, eyeliner, sunscreen ans many more. 
+This project  is built in the span of 5 day.
+
+***
+## Tech Stacks I have used  is:-
+
+### `React`
+### `Javacript`
+### `Chakra-Ui`
+### `Cascading Style Sheet (CSS)`
+### `NPM packages`
+### `JSON-serverr`
+
+***
+
+##  Features 
+
+ * Login and Signup User Account
+ * Authentication using Private Route
+ * Various variety of products, like makeup, haircare,etc
+ * Product sorting on teh basis of Price
+ * Add to bag option with Bag Counter on the top of navbar
+ * Bag product Quantity can change by increasing and removing it
+ * checkout page with GST added to payable amount.
+
+
+***
+
+
+## Images of the GG.com
+
+## Landing Page
+
+![LandingPage1](/images/LandingPage1.png)
+![LandingPage2](/images/LandingPage2.png)
+![responsive](/images/Rsposiveness.png)
+
+ ## Sign up
+
+![signup](/images/signup.png)
+
+ ## Product Page
+
+![ProductPage](/images/ProductPage.png) 
+ 
+## Product Page
+
+![BagPage](/images/Bag20Page.png) 
+
+## Checkout Page
+
+![checkout](/images/Checkout.png) 
+
+
+
